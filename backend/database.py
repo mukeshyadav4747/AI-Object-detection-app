bcf4887f9e4809f444aa9357bb29a3177e7a4b3a
appel <appel@appels-MacBook-Pro.local>
2025-09-25 12:10:03 +0530

    file_updated

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,16 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DB_USER = "root"
-DB_PASSWORD = "yadav4747"
-DB_HOST = "localhost"
-DB_NAME = "object_detection_db"
+# Load environment variables from .env file
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
